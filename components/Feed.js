535209a53be34245aa6599867644dc82de4e06cb
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2023-02-16 08:52:58 -0800

    leftSidebar

---
+++ components/Feed.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import LeftSidebar from './LeftSidebar'
+
+const Feed = () => {
+  return (
+    <div>
+      {/* Leftsidebar */}
+      <LeftSidebar />
+      {/* stories */}
+      {/* create post */}
+      {/* posts */}
+      {/* rightSide */}
+    </div>
+  )
+}
+
+export default Feed
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 
 import { Inter } from '@next/font/google'
 import Header from 'components/Header'
+import Feed from 'components/Feed'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,6 +19,7 @@ export default function Home() {
     {/* Header */}
       <Header />
     {/* Feed */}
+    <Feed />
     </>
   )
 }
